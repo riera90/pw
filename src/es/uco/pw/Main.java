b75e9d2dbc6f926e6bbd6bbb4fc2902cba0aec66
diego@alpha <diegorodriguezriera@gmail.com>
2020-11-15 02:38:55 +0100

    added basic sql connector, and updated the buuld script

--- src/es/uco/pw/Main.java
+++ src/es/uco/pw/Main.java
@@ -1,8 +1,12 @@
 package es.uco.pw;
 
 import es.uco.pw.business.Daemons.FlashPostDaemon;
+import es.uco.pw.business.dao.common.DBConn;
 import es.uco.pw.display.CLI.CliUserInterface;
 import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * The type Main.
@@ -15,12 +19,24 @@ public class Main {
      * @throws NoSuchAlgorithmException the no such algorithm exception
      */
     public static void main(String[] args) throws NoSuchAlgorithmException {
+        try {
+            Connection conn = DBConn.getConn();
+            ResultSet rs = DBConn.execQuery(conn, "select * from pw.User");
+            while (rs.next()){
+                System.out.println(rs.getString("firstName"));
+            }
+            conn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
+        /*
         FlashPostDaemon flashPostDaemon = new FlashPostDaemon("flashPostDaemon");
         flashPostDaemon.start();
 
         CliUserInterface.init();
 
         flashPostDaemon.stopThread();
+        */
     }
 }
--- src/es/uco/pw/business/dao/common/DBConn.java
+++ src/es/uco/pw/business/dao/common/DBConn.java
@@ -1,4 +1,52 @@
 package es.uco.pw.business.dao.common;
 
+import java.io.*;
+import java.sql.*;
+import java.util.Properties;
+
 public class DBConn {
+    public DBConn(){
+
+    }
+
+    static public Connection getConn() throws SQLException, ClassNotFoundException {
+        String user = "";
+        String password = "";
+        String address = "";
+        Connection conn = null;
+        try {
+            InputStream in = DBConn.class.getResourceAsStream("/.properties");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            Properties p = new Properties();
+            p.load(reader);
+            user = p.getProperty("MYSQL_USER");
+            password = p.getProperty("MYSQL_PASSWORD");
+            address = p.getProperty("MYSQL_ADDRESS");
+        } catch (NullPointerException e){
+            try {
+                FileReader reader=new FileReader(".properties");
+                Properties p = new Properties();
+                p.load(reader);
+                user = p.getProperty("MYSQL_USER");
+                password = p.getProperty("MYSQL_PASSWORD");
+                address = p.getProperty("MYSQL_ADDRESS")+"/"+p.getProperty("MYSQL_DATABASE");
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection(address, user, password);
+    }
+
+    static public ResultSet execQuery(Connection conn, String query) throws SQLException, ClassNotFoundException {
+        Statement stmt= null;
+        ResultSet result = null;
+        assert conn != null;
+        stmt = conn.createStatement();
+        result = stmt.executeQuery(query);
+        return result;
+    }
 }
