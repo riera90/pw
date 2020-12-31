c884ec1a5a2fba8f835ef7d61c554f468a46bc35
diego@alpha <diegorodriguezriera@gmail.com>
2020-12-31 16:34:01 +0100

    modified deployment by adding apache path and app name

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,27 @@
 # execute build
 ./build.sh
 
-mkdir -p /opt/apache-tomcat-8.5.60/webapps/pw
+source config.properties
+
+mkdir -p $APACHE_DIR/webapps/$APP_NAME
 
 # clean old files
-rm /opt/apache-tomcat-8.5.60/webapps/pw/* -rf
+rm $APACHE_DIR/webapps/$APP_NAME/* -rf
 
 # add both jar and war
-cp artifacts/es.uco.pw.war /opt/apache-tomcat-8.5.60/webapps/pw
-# cp artifacts/es.uco.pw.jar /opt/apache-tomcat-8.5.60/webapps/es/uco/pw
+cp artifacts/es.uco.pw.war $APACHE_DIR/webapps/$APP_NAME
+# cp artifacts/es.uco.pw.jar $APACHE_DIR/webapps/$APP_NAME
 
 # change directory to deploy folder
-cd /opt/apache-tomcat-8.5.60/webapps/pw
+cd $APACHE_DIR/webapps/$APP_NAME
 
 # extract the war
 jar xvf es.uco.pw.war
 rm es.uco.pw.war
 
 # restart tomcat
-/opt/apache-tomcat-8.5.60/bin/shutdown.sh
-/opt/apache-tomcat-8.5.60/bin/startup.sh
+$APACHE_DIR/bin/shutdown.sh
+$APACHE_DIR/bin/startup.sh
 
 
 
@@ -28,5 +30,5 @@ rm es.uco.pw.war
 #kill -9 $(ps aux | grep google/chrome | head -n 1 | sed -r 's/[a-zA-Z]*\ *([0-9]{2,})\ *.*/\1/g')
 #sudo lsof -i -n -P | grep LISTEN | grep java
 sleep 1
-firefox --private-window http://localhost:8080/pw
-#watch tail -n 30 /opt/apache-tomcat-8.5.60/logs/catalina.out
+firefox --private-window http://localhost:8080/$APP_NAME
+#watch tail -n 30 $APACHE_DIR/logs/catalina.out
